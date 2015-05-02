d03fce3a05388a67f71fed45719c76657963af2f
Christopher D. Long <galizur@gmail.com>
2015-05-02 17:40:13 -0400

    Boxing update.

--- sos/current_ranking.sql
+++ sos/current_ranking.sql
@@ -6,8 +6,8 @@ exp(bf.estimate)::numeric(6,2) as str
 from boxrec.boxers b
 join boxrec._basic_factors bf
   on (bf.factor, bf.level) = ('boxer', b.id::text)
-order by str desc
-limit 50;
+order by str desc;
+--limit 50;
 
 copy (
 select
@@ -17,7 +17,7 @@ from boxrec.boxers b
 join boxrec._basic_factors bf
   on (bf.factor, bf.level) = ('boxer', b.id::text)
 order by str desc
-limit 50
+--limit 50
 ) to '/tmp/current_ranking.csv' csv header;
 
 commit;
