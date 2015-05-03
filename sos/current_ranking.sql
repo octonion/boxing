18677f32953ab743aa971407f40ce60fd7e77e48
Christopher D. Long <galizur@gmail.com>
2015-05-03 13:00:52 -0400

    Boxing update.

--- sos/current_ranking.sql
+++ sos/current_ranking.sql
@@ -2,22 +2,24 @@ begin;
 
 select
 coalesce(b.boxer_name) as name,
-exp(bf.estimate)::numeric(6,2) as str
-from boxrec.boxers b
-join boxrec._basic_factors bf
-  on (bf.factor, bf.level) = ('boxer', b.id::text)
-order by str desc;
---limit 50;
-
-copy (
-select
-coalesce(b.boxer_name) as name,
+replace(replace(b.division,'"',''),'+',' ') as division,
 exp(bf.estimate)::numeric(6,2) as str
 from boxrec.boxers b
 join boxrec._basic_factors bf
   on (bf.factor, bf.level) = ('boxer', b.id::text)
 order by str desc
---limit 50
+limit 250;
+
+copy (
+select
+coalesce(b.boxer_name) as name,
+replace(replace(b.division,'"',''),'+',' ') as division,
+exp(bf.estimate)::numeric(6,2) as str
+from boxrec.boxers b
+join boxrec._basic_factors bf
+  on (bf.factor, bf.level) = ('boxer', b.id::text)
+order by str desc
+limit 250
 ) to '/tmp/current_ranking.csv' csv header;
 
 commit;
